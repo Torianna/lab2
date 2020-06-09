61175c2036e33d4b462434a1cff037c32499c21b
Klaudia <klaudia.goralska95@gmail.com>
2020-06-09 18:43:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("Lab2","metoda onCreate");
+        Toast.makeText(this, "Metoda onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("lab2","metoda onResume");
+        Toast.makeText(this,"Metoda onResume",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d("lab2","metoda onPause");
+        Toast.makeText(this,"Metoda onPause",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d("lab2","metoda onStop");
+        Toast.makeText(this,"Metoda onStop",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d("lab2","metoda onDestroy");
+        Toast.makeText(this,"Metoda onDestroy",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.d("lab2","metoda onRestart");
+        Toast.makeText(this,"Metoda onRestart",Toast.LENGTH_SHORT).show();
+    }
+}
+
+
